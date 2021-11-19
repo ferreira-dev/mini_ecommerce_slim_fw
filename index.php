c43996731a8a1dfa8bafcf7e0907ad970a099cad
Fabricio Ferreira <fabroide@gmail.com>
2021-11-19 19:32:45 -0300

    #login admin e gitignore

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-	
+$app->get('/', function () {
+
 	$page = new Page();
 	$page->setTpl("index");
-
 });
 
 $app->get('/admin', function () {
@@ -25,12 +25,19 @@ $app->get('/admin', function () {
 $app->get('/admin/login', function () {
 
 	$page = new PageAdmin([
-		"header"=>false,
-		"footer"=>false,
+		"header" => false,
+		"footer" => false,
 	]);
 
 	$page->setTpl("login");
+});
 
+$app->post('/admin/login', function () {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("/Location: /admin");
+	exit;
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class User extends Model {
+    public static function login($Login, $password)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+            ":LOGIN"=>$login
+        ));
+
+        if (count($results) === 0) {
+            throw new \Exception("Usuário inexistente ou senha inválida.");
+        }
+
+        $data = $results[0];
+
+        if (password_verify($password, $data["despasswotd"]) === true) {
+
+            $user = new User();
+
+            $user->setIdUser($data["iduser"]);
+
+        } else {
+            throw new \Exception("Usuário inexistente ou senha inválida");
+        }
+
+    }
+}
